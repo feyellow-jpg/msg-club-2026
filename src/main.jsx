cfdb42a5a58c51a04ff9683c3f277426a8432f17
feyellow-jpg <feyellow@eulsukdo.es.kr>
2026-05-07 17:50:32 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// React 앱을 index.html의 'root' 엘리먼트에 렌더링합니다.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
